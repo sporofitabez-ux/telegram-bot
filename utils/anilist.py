2ff3d723a5f8f04725711cbc6552bccea58c1c03
sporofitabez-ux <sporofitabez@gmail.com>
2026-02-22 21:33:08 +0200

    Update anilist.py

--- utils/anilist.py
+++ utils/anilist.py
@@ -3,16 +3,47 @@ import re
 
 ANILIST_URL = "https://graphql.anilist.co"
 
+TRANSLATE_URL = "https://translate.googleapis.com/translate_a/single"
 
-# remove html da sinopse
+_translation_cache = {}
+
+
 def clean_html(text):
     return re.sub("<.*?>", "", text or "")
 
 
-# resumo simples automático
-def summarize(text, max_sentences=3):
+def summarize(text, max_sentences=4):
     sentences = text.split(". ")
     return ". ".join(sentences[:max_sentences]).strip() + "."
+
+
+def is_english(text):
+    common_words = ["the ", " is ", " of ", " and ", " in ", " to "]
+    text_lower = text.lower()
+    return any(word in text_lower for word in common_words)
+
+
+async def translate_to_pt(text):
+
+    if text in _translation_cache:
+        return _translation_cache[text]
+
+    params = {
+        "client": "gtx",
+        "sl": "en",
+        "tl": "pt",
+        "dt": "t",
+        "q": text,
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(TRANSLATE_URL, params=params) as resp:
+            result = await resp.json()
+
+    translated = "".join([item[0] for item in result[0]])
+
+    _translation_cache[text] = translated
+    return translated
 
 
 async def search_anilist(title):
@@ -23,8 +54,9 @@ async def search_anilist(title):
         title {
           romaji
           english
+          native
         }
-        description(asHtml:false)
+        description(asHtml: false)
         genres
         coverImage {
           extraLarge
@@ -38,18 +70,29 @@ async def search_anilist(title):
             ANILIST_URL,
             json={"query": query, "variables": {"search": title}},
         ) as resp:
-
             data = await resp.json()
 
     media = data["data"]["Media"]
 
-    synopsis = clean_html(media["description"])
+    synopsis = clean_html(media.get("description", ""))
+
+    if not synopsis:
+        synopsis = "Sem sinopse disponível."
+
+    # 🔥 FALLBACK AUTOMÁTICO
+    if is_english(synopsis):
+        try:
+            synopsis = await translate_to_pt(synopsis)
+        except:
+            pass
+
     synopsis = summarize(synopsis)
 
     return {
         "title": media["title"]["romaji"]
-        or media["title"]["english"],
-        "genres": ", ".join(media["genres"]),
+        or media["title"]["english"]
+        or media["title"]["native"],
+        "genres": ", ".join(media.get("genres", [])),
         "cover": media["coverImage"]["extraLarge"],
         "synopsis": synopsis,
     }
