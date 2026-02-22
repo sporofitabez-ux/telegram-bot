d6154222216c2b733822cb6bcb74bf78a870946a
sporofitabez-ux <sporofitabez@gmail.com>
2026-02-22 19:29:10 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,15 +1,11 @@
 from sources.toonbr import ToonBrSource
 from sources.mangaflix import MangaFlixSource
 
-# Dicionário de fontes disponíveis
+# fontes disponíveis
 _sources = {
     "ToonBr": ToonBrSource(),
     "MangaFlix": MangaFlixSource()
 }
 
 def get_all_sources():
-    """
-    Retorna todas as fontes disponíveis no formato:
-    { "NomeDaFonte": FonteClass() }
-    """
     return _sources
